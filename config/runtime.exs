81830eb16845a9ab5061fb96ed1520257bf83250
codewave <info.devforge@gmail.com>
2026-01-27 11:40:05 +0300

    com

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,119 +1,40 @@
 import Config
 
-# config/runtime.exs is executed for all environments, including
-# during releases. It is executed after compilation and before the
-# system starts, so it is typically used to load production configuration
-# and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
-
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/first start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :first, FirstWeb.Endpoint, server: true
-end
-
-config :first, FirstWeb.Endpoint, http: [port: String.to_integer(System.get_env("PORT", "4000"))]
-
 if config_env() == :prod do
-  # POSTGRES CONFIGURATION
+  # Force Phoenix server to start
+  config :first, FirstWeb.Endpoint, server: true
+
+  # DATABASE
   database_url =
     System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: postgres://USER:PASS@HOST/DATABASE
-      """
+      raise "DATABASE_URL is missing!"
 
-  pool_size = String.to_integer(System.get_env("POOL_SIZE") || "10")
+  ssl_enabled = System.get_env("DB_SSL") == "true"
+
+  ssl_opts =
+    if ssl_enabled, do: [verify: :verify_none], else: []
 
   config :first, First.Repo,
     url: database_url,
-    pool_size: pool_size,
-    # recommended for Fly.io Postgres
-    ssl: true
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "15"),
+    ssl: ssl_enabled,
+    ssl_opts: ssl_opts,
+    queue_target: 5000,
+    queue_interval: 1000
 
-  # The secret key base is used to sign/encrypt cookies and other secrets.
+  # SECRET KEY
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+      raise "SECRET_KEY_BASE is missing!"
 
-  host = System.get_env("PHX_HOST") || "example.com"
-
-  config :first, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
-
-  maybe_ipv6 =
-    case System.get_env("PHX_IPV6") do
-      "true" -> [:inet6]
-      _ -> []
-    end
+  # PHX HOST
+  host = System.get_env("PHX_HOST") || "first-dark-sea-8115.fly.dev"
 
   config :first, FirstWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: 4000
+      port: String.to_integer(System.get_env("PORT") || "4000")
     ],
-    server: true,
-    secret_key_base: secret_key_base,
-    socket_options: maybe_ipv6
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :first, FirstWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :first, FirstWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Here is an example configuration for Mailgun:
-  #
-  #     config :first, First.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # Most non-SMTP adapters require an API client. Swoosh supports Req, Hackney,
-  # and Finch out-of-the-box. This configuration is typically done at
-  # compile-time in your config/prod.exs:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Req
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+    secret_key_base: secret_key_base
 end
