945ab154c4333f80d60f47c4cedb343f9d721e06
codewave <info.devforge@gmail.com>
2026-01-27 12:40:27 +0300

    first

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,40 +1,41 @@
 import Config
 
+# Enable Phoenix server always in prod
 if config_env() == :prod do
-  # Force Phoenix server to start
   config :first, FirstWeb.Endpoint, server: true
+end
 
-  # DATABASE
+# Use the Fly PORT env if available
+config :first, FirstWeb.Endpoint,
+  http: [
+    ip: {0, 0, 0, 0, 0, 0, 0, 0},
+    port: String.to_integer(System.get_env("PORT") || "4000")
+  ]
+
+if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
-      raise "DATABASE_URL is missing!"
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
 
-  ssl_enabled = System.get_env("DB_SSL") == "true"
-
-  ssl_opts =
-    if ssl_enabled, do: [verify: :verify_none], else: []
+  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :first, First.Repo,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "15"),
-    ssl: ssl_enabled,
-    ssl_opts: ssl_opts,
-    queue_target: 5000,
-    queue_interval: 1000
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    socket_options: maybe_ipv6
 
-  # SECRET KEY
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
-      raise "SECRET_KEY_BASE is missing!"
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      """
 
-  # PHX HOST
-  host = System.get_env("PHX_HOST") || "first-dark-sea-8115.fly.dev"
+  host = System.get_env("PHX_HOST") || "example.com"
 
   config :first, FirstWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    http: [
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
-    ],
     secret_key_base: secret_key_base
 end
